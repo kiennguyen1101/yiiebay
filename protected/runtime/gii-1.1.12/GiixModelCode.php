10c9f45d78ed2c36b3fd46d961f68e556291142e
kiennguyen1101 <kiennguyen1101@gmail.com>
2013-04-19 17:45:45 +0700

    remove cache for .gitignore

--- protected/runtime/gii-1.1.12/GiixModelCode.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  'template' => 'default',
-  'connectionId' => 'db',
-  'tablePrefix' => 'yii_',
-  'modelPath' => 'application.models',
-  'baseClass' => 'GxActiveRecord',
-  'buildRelations' => true,
-);
